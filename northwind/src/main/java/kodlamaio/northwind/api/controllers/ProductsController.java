76f08ac520237b6d0deaffa9203b7a9f625b60e0
gohenka <gohenka@gmail.com>
2021-05-30 23:22:40 +0300

    30.05.2021

--- northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
+++ northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -14,6 +14,7 @@ import kodlamaio.northwind.business.abstracts.ProductService;
 import kodlamaio.northwind.core.utilities.results.DataResult;
 import kodlamaio.northwind.core.utilities.results.Result;
 import kodlamaio.northwind.entities.concretes.Product;
+import kodlamaio.northwind.entities.dtos.ProductWithCategoryDto;
 
 @RestController
 @RequestMapping("/api/products")
@@ -53,5 +54,10 @@ public class ProductsController {
 	public DataResult<List<Product>> getAllSorted() {
 		return this.productService.getAllSorted();
 	}
-
+	
+	@GetMapping("/getproductwithcategorydetail")
+	public DataResult<List<ProductWithCategoryDto>> getProductWithCategoryDetails()
+	{
+		return this.productService.getProductWithCategoryDetails();
+	}
 }
--- northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
+++ northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
@@ -7,10 +7,14 @@ import org.springframework.data.jpa.repository.Query;
 import kodlamaio.northwind.core.utilities.results.DataResult;
 import kodlamaio.northwind.core.utilities.results.Result;
 import kodlamaio.northwind.entities.concretes.Product;
+import kodlamaio.northwind.entities.dtos.ProductWithCategoryDto;
 
 public interface ProductService {
+	
 	DataResult<List<Product>> getAll();
+	
 	DataResult<List<Product>> getAllSorted();
+	
     DataResult<List<Product>> getAll(int pageNo, int pageSize);
 	Result add(Product product);
 	
@@ -27,6 +31,7 @@ public interface ProductService {
 	DataResult<List<Product>> getByProductNameStartsWith(String productName);
 	
 	DataResult<List<Product>> getByNameAndCategory(String productName,int categoryId);
-
+	
+	DataResult<List<ProductWithCategoryDto>> getProductWithCategoryDetails();
 
 }
